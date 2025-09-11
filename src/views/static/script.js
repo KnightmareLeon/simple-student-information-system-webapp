0c233e648be32f9922967d7582e79e4b35f65d3d
Knightmare Leon <leonardjohncorpuz20@gmail.com>
2025-09-11 12:33:10 +0800

    refactor(views): use bootstrap-select plugin instead for autocomplete dropdown menus

--- src/views/static/script.js
+++ src/views/static/script.js
@@ -5,19 +5,3 @@ toggleHeader.addEventListener('click', () => {
     sidebar.classList.toggle('minimized');
     document.cookie = "sidebar=" + (sidebar.classList.contains('minimized') ? "minimized" : "expanded") + ";path=/";
 });
-
-$('#filter').editableSelect({
-	effects: 'slide',
-	duration: 200,
-    filter: false
-});
-
-$('#recordCollegeCode').editableSelect({
-	effects: 'slide',
-	duration: 200,
-});
-
-$('#recordProgramCode').editableSelect({
-	effects: 'slide',
-	duration: 200,
-});
