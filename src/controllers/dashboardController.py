7b5c5ece03daf402f7b58a6f876a619015771ed4
Knightmare Leon <leonardjohncorpuz20@gmail.com>
2025-09-12 15:38:14 +0800

    feat(dashboard): add content cards showing total number of students, programs, and colleges

--- src/controllers/dashboardController.py
+++ src/controllers/dashboardController.py
@@ -4,4 +4,4 @@ dashboard_bp = Blueprint("dashboard", __name__)
 
 @dashboard_bp.route("/")
 def index():
-    return render_template("dashboard.html", active_page = "dashboard")
+    return render_template("dashboard/index.html", active_page = "dashboard", total_stds=50, total_prgs=5, total_clgs=2)
